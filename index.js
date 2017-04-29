b0a99a01e59717537028489287dce53a04a2a4e4
Benjamin Congdon <bencongdon96@gmail.com>
2017-04-29 09:09:18 -0500

    Charting command

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var speedtest = require('speedtest-net');
 var sqlite3 = require('sqlite3');
 var program = require('commander');
 var fs = require('fs');
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
 
 var db = new sqlite3.Database(__dirname + '/data.db');
 
@@ -12,7 +14,7 @@ var create_table = 'CREATE TABLE IF NOT EXISTS speedtest ( ' +
 var insert = 'INSERT INTO speedtest(time, upload, download, ping) VALUES (?, ?, ?, ?)';
 var clear_table = 'DELETE FROM speedtest';
 var stats_sql = 'SELECT AVG(upload), MAX(upload), ' + 
-  'AVG(download), MAX(download), AVG(ping), MAX(ping) FROM speedtest;'
+  'AVG(download), MAX(download), AVG(ping), MAX(ping) FROM speedtest;';
 var all_data = 'SELECT * from speedtest';
 
 function doSpeedTest(cb) {
@@ -63,10 +65,76 @@ program
 program
   .command('stats')
   .description('Displays speedtest stats')
+  .option('-l, --last [day, week, month]', 'Stats time period to consider')
   .action(() => {
     db.get(stats_sql, (err, data) => {
       if(err) throw err;
       console.log(data);
+    });
+  });
+
+program
+  .command('chart')
+  .description('Shows chart of measurements over time')
+  .option('-l, --last [day, week, month]', 'Stats time period to consider')
+  .action(() => {
+    db.all(all_data, (err, data) => {
+      if(err) throw err;
+      var screen = blessed.screen();
+      var grid = new contrib.grid({rows: 2, cols: 2, screen: screen});
+
+      // Setup ping line chart
+      var ping_line = grid.set(0, 0, 1, 1, contrib.line, {
+        style: {
+          line: 'yellow',
+          text: 'green',
+          baseline: 'black'
+        },
+        showLegend: true,
+        xLabelPadding: 3,
+        xPadding: 5,
+        label: 'Ping (ms)'
+      });
+      var ping_series = {
+        title: 'Ping',
+        x: data.map((d) => {return d.time;}),
+        y: data.map((d) => {return d.ping;})
+      }
+      ping_line.setData([ping_series]);
+
+      // Setup upload/download chart
+      var updown_line = grid.set(0, 1, 1, 1, contrib.line, {
+        style: {
+          text: 'green',
+          baseline: 'black'
+        },
+        showLegend: true,
+        xLabelPadding: 3,
+        xPadding: 5,
+        label: 'Upload/Download Speed (Mb/s)'
+      });
+      var up_series = {
+        title: 'Upload',
+        x: data.map((d) => {return d.time;}),
+        y: data.map((d) => {return d.upload;}),
+        style: {
+          line: 'blue'
+        }
+      }
+      var down_series = {
+        title: 'Download',
+        x: data.map((d) => {return d.time;}),
+        y: data.map((d) => {return d.download;}),
+        style: {
+          line: 'green'
+        }
+      }
+      updown_line.setData([up_series, down_series]);
+
+      screen.key(['escape', 'q', 'C-c'], () => {
+        return process.exit(0);
+      });
+      screen.render();
     });
   });
 
